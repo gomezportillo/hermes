5658920ccf5bf36190b826e6165e1b93e1bc7bfc
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2016-06-29 12:35:07 +0200

    project refactorization

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Embedded real-time chat and YouTube player using Node.js and bootstrap
 
 1. Install [Node.js](https://nodejs.org/)
-2. Install dependecies by
+2. Install dependecies as
 ```
-npm Install
+npm install
 ```  
-3. Execute program by
+3. Execute program as
 ```
-node app.js
+node index.js
 ```
---
+++ misc/requestHandlers.js
@@ -0,0 +1,66 @@
+var exec = require("child_process").exec;
+var fs = require("fs");
+
+function subir(response, data)
+{
+  response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+  data = data.toString("utf-8")
+  response.write("¿Has dicho "+data+"?")
+  response.end();
+}
+
+function iniciar(response, data)
+{
+  fs.readFile('pages/student.html', 'utf8', function (err, data)
+  {
+    if (err)
+    {
+      console.log(err);
+    }
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.write(data)
+    response.end()
+  });
+}
+
+function getFiles(response, data)
+{
+  exec("dir /b", function (error, stdout, stderr)
+  {
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.write("<p>Estos son los archivos del servidor:</p>")
+    response.write("<p>"+stdout+"</p>")
+    response.end()
+  });
+}
+
+function student(response, data) {}
+
+function professor(response, data)
+{
+  fs.readFile('pages/professor.html', 'utf8', function (err, data)
+  {
+    if (err)
+    {
+      console.log(err)
+    }
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.write(data)
+    response.end()
+  });
+}
+
+function notFound(pathname, response)
+{
+  response.writeHead(404, {"Content-Type": "text/html"})
+  response.write("<p>404 - No se ha encontrado la ruta. Ir a <a href=/iniciar>inicio</a></p>")
+  response.end()
+}
+
+
+exports.iniciar = iniciar;
+exports.subir = subir;
+exports.getFiles = getFiles;
+exports.student = student;
+exports.student = professor;
+exports.notFound = notFound;
