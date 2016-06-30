9af616ed0ba3d7ea0a533d3aba6ee209678b7244
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2016-06-30 13:34:48 +0200

    launcher updating url and runnig nodejs server

--- launcher.py
+++ launcher.py
@@ -4,7 +4,9 @@
 import os
 import sys
 import socket
-from string import Template
+import codecs
+import subprocess
+import thread
 
 try:
     import gtk
@@ -47,7 +49,13 @@ class HermesApp:
             button.set_label('Detener servidor')
 
             url = self.builder.get_object('url-entry').get_text()
-            self.set_youtube_url(url)
+            if (url != ""):
+                self.set_youtube_url(url)
+
+            cmd_line = "node index.js"
+            p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
 
         else:
             self.state = State.stopped
@@ -69,11 +77,14 @@ class HermesApp:
         return socket.gethostbyname(hostname)
 
     def set_youtube_url(self, url):
-        html_template_file = open(os.path.join('pages', 'student_template.html'))
-        html_template = Template(html_template_file.read())
-        updated_html_content = html_template.substitute(VIDEO_URL=url)
+        url = url.split('v=')[1].split('&')[0]
+        url = 'http://youtube.com/embed/' + url
 
-        update_html_file = open(os.path.join('pages', 'student.html'))
+        html_template_file = codecs.open(os.path.join('pages', 'student_template.html'), encoding='utf8')
+        html_template = html_template_file.read()
+        updated_html_content = html_template.replace('$VIDEO_URL', url)
+
+        update_html_file = os.path.join('pages', 'student.html')
         with open(update_html_file, 'w+') as new_html_file:
             new_html_file.write(updated_html_content)
 
