49b94c670ee4e5ba17b0ef268b067111bedf89a9
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2016-07-13 12:57:08 +0200

    receiving user and pwd and checking on users.csv

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require('fs');
-var nicknames = {};
+var connected_users = {};
+var registered_users = {};
 
 
 app.use(express.static(__dirname + '/images'));
@@ -25,18 +26,25 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('new user', function(user, password, callback) {
-        if (user in nicknames) {
+        if (user in connected_users) { //si el estudiante ya está conectado
             callback(false);
         } else {
-            callback(true);
-            socket.nickname = user;
-            nicknames[socket.nickname] = true;
-            updateNickNames();
+            read_registered_users();
+            console.log(user);
+            console.log(password);
+            if (user in registered_users && registered_users[user] == password) { //si nombre y contraseña coinciden
+                callback(true);
+                socket.nickname = user;
+                connected_users[socket.nickname] = 'connected';
+                update_users();
+            } else {
+                callback(false);
+            }
         }
     });
 
-    socket.on('log in', function(password, callback) {
-        console.log("Intentando acceder con la contraseña" + password);
+    socket.on('log professor', function(password, callback) {
+        console.log("Profesor intentando acceder con la contraseña " + password);
         if (password == 'cted') {
             callback(true);
         } else {
@@ -56,12 +64,12 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('disconnect', function(data) {
         if(!socket.nickname) return;
-        delete nicknames[socket.nickname];
-        updateNickNames();
+        delete connected_users[socket.nickname];
+        update_users();
     });
 
-    function updateNickNames() {
-        io.sockets.emit('usernames', nicknames);
+    function update_users() {
+        io.sockets.emit('usernames', connected_users);
     }
 
     function splitURL(url) {
@@ -79,5 +87,20 @@ io.sockets.on('connection', function(socket) {
                 console.log('Updated URL ' + newURL);
             });
         });
+    }
+
+    function read_registered_users() {
+        fs.readFile('users.csv', 'utf-8', function(err, data) {
+            if (err) throw err;
+
+            var array = data.split('\n');
+            array.forEach(function(item) {
+                var splitted_item = item.split(';');
+                if (splitted_item != '') {
+                    registered_users[splitted_item[0]] = splitted_item[1].split('\r')[0];
+                }
+            });
+        });
+        console.log(registered_users);
     }
 });
