fb0092952a5382678bcfbeab72f03bac41c61022
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2016-07-25 11:56:28 +0200

    only registered users can access to functionalities
    this includes send/receive messages and view connected users

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var io = require("socket.io").listen(server);
 var fs = require('fs');
 var connected_users = {};
 var registered_users = {};
+var professor_nickname = '¬HERMES:professor'
 
 
 app.use(express.static(__dirname + '/images'));
@@ -22,19 +23,27 @@ app.get('/admin', function(req, res) {
 io.sockets.on('connection', function(socket) {
 
     socket.on('send message', function(data) {
-        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+        if (connected_users[socket.nickname] == true || socket.nickname == professor_nickname) { //si el usuario que manda está conectado
+            var sender_name = socket.nickname;
+
+            io.sockets.clients().forEach(function (socket) {
+                if (connected_users[socket.nickname] == true || socket.nickname == professor_nickname) { //si el usuario que recibe está conectado
+                    socket.emit('new message', {msg: data, nick: sender_name});
+                }
+            });
+        }
     });
 
     socket.on('new user', function(user, password, callback) {
-        if (user in connected_users) { //si el estudiante ya está conectado
+        if (connected_users[user] = true) { //si el estudiante ya está conectado
             callback(false);
         } else {
-            read_registered_users();
-            if (user in registered_users && registered_users[user] == password) { //si nombre y contraseña coinciden
+            load_csv_users_file();
+            if (registered_users[user] == password) { //si nombre y contraseña coinciden
+                socket.nickname = capitalizeFirstLetter(user);
+                connected_users[socket.nickname] = true;
+                announce_users();
                 callback(true);
-                socket.nickname = user;
-                connected_users[socket.nickname] = 'connected';
-                io.sockets.emit('user connected', connected_users);
             } else {
                 callback(false);
             }
@@ -42,9 +51,9 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('log professor', function(password, callback) {
-        console.log("Profesor intentando acceder con la contraseña " + password);
         if (password == 'cted') {
-            socket.nickname = '¬HERMES:profesor'
+            socket.nickname = professor_nickname;
+            announce_users();
             callback(true);
         } else {
             callback(false);
@@ -52,39 +61,53 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('update video', function(url, callback) {
-        url = splitURL(url);
-        if (typeof url != 'undefined') {
-            updateWebPage(url);
-            callback(true);
-        } else {
-            callback(false);
+        var correct = '';
+        try {
+            correct = updateWebPage(url);
+        } catch (ex) {
+            console.log("Error actualizando la URL en las página del alumno.");
+            correct = false;
         }
+        callback(correct);
     });
 
     socket.on('disconnect', function(data) {
         if(!socket.nickname) return;
         delete connected_users[socket.nickname];
-        io.sockets.emit('user connected', connected_users);
+        callback(true);
+        announce_users();
     });
 
-    function splitURL(url) {
-        return url.split("v=")[1]
+    function updateWebPage(newURL) {
+        newURL = splitURL(newURL);
+        if (typeof url != 'undefined') {
+
+            fs.readFile('pages/student_template.html', 'utf-8', function(err, data){
+                if (err) throw err;
+
+                var new_file = data.replace('$VIDEO_URL', newURL);
+
+                fs.writeFile('pages/student.html', new_file, 'utf-8', function (err) {
+                    if (err) throw err;
+
+                    console.log('Updated URL ' + newURL);
+                    return true;
+                });
+            });
+        } else {
+            return false;
+        }
     }
 
-    function updateWebPage(newURL) {
-        fs.readFile('pages/student_template.html', 'utf-8', function(err, data){
-            if (err) throw err;
-
-            var new_file = data.replace('$VIDEO_URL', newURL);
-
-            fs.writeFile('pages/student.html', new_file, 'utf-8', function (err) {
-                if (err) throw err;
-                console.log('Updated URL ' + newURL);
-            });
+    function announce_users(){
+        io.sockets.clients().forEach(function (socket) {
+            if (connected_users[socket.nickname] == true) { //si el usuario que recibe está conectado
+                socket.emit('user connected', connected_users);
+            }
         });
     }
 
-    function read_registered_users() {
+    function load_csv_users_file() {
         fs.readFile('users.csv', 'utf-8', function(err, data) {
             if (err) throw err;
 
@@ -92,10 +115,17 @@ io.sockets.on('connection', function(socket) {
             array.forEach(function(item) {
                 var splitted_item = item.split(';');
                 if (splitted_item != '') {
-                    registered_users[splitted_item[0]] = splitted_item[1].split('\r')[0];
+                    registered_users[splitted_item[0]] = splitted_item[1].split('\r')[0]; //all_users[user] = password
                 }
             });
         });
-        console.log(registered_users);
+    }
+
+    function splitURL(url) {
+        return url.split("v=")[1]
+    }
+    
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 });
